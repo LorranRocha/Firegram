59e583f54b887ceb40a54d03417fe5a4f2103801
LorranRocha <youtubebizarro@hotmail.com>
2022-03-13 16:22:57 -0300

    Updates

---
+++ precache-manifest.51b0606c83d2a626613e89f26236ba35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c896a2a54855cd8f9c4ef58c6725ef",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "449bbedcfcb92f4de328",
+    "url": "/firegram/static/css/main.52d9037e.chunk.css"
+  },
+  {
+    "revision": "2393bcf96e10c2323ce7",
+    "url": "/firegram/static/js/2.316a4fb1.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/firegram/static/js/2.316a4fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449bbedcfcb92f4de328",
+    "url": "/firegram/static/js/main.803fe4bc.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
